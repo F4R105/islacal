9bffd033cc2b17954861e234fde8cc6f4f8537f2
F4R105 <kajubufaraji@gmail.com>
2025-07-11 18:03:10 +0300

    add assets, icons and images

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,50 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { AppDataProvider } from '@/providers/AppDataProvider';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useEffect, useCallback, useState } from 'react';
+import { View } from 'react-native';
+
+// Prevent splash from auto-hiding
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const [appReady, setAppReady] = useState(false);
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  useEffect(() => {
+    if (fontsLoaded) {
+      setAppReady(true);
+    }
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appReady) {
+      // SplashScreen.hideAsync().catch(() => {});
+    }
+  }, [appReady]);
+
+  if (!appReady) return null;
 
   return (
-    // <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <AppDataProvider>
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="error" />
             <Stack.Screen name="+not-found" />
           </Stack>
           <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
         </AppDataProvider>
       </ThemeProvider>
-    // </SafeAreaView>
+    </View>
   );
 }
